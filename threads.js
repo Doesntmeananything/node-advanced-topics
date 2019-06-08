11d96198724888f57893642970ff2c773472c158
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-08 08:50:24 +0300

    Update comments

--- threads.js
+++ threads.js
@@ -2,7 +2,7 @@ const crypto = require("crypto");
 
 const start = Date.now();
 // Sample hashing functions where
-// we check how long it will take to run
+// we check how long they will take to run
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("1:", Date.now() - start);
 });
