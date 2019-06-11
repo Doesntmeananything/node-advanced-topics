373a0e1fc32582181efcbd833a6a39341d32266f
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-11 20:54:19 +0300

    Add clustering suitable for PM2

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require("cluster");
+// Child that acts like a server
+const crypto = require("crypto");
+const express = require("express");
+const app = express();
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Execute index.js again in child mode
-  cluster.fork();
-  cluster.fork();
-} else {
-  // Child that acts like a server
-  const crypto = require("crypto");
-  const express = require("express");
-  const app = express();
-
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("Hi there");
-    });
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("Hi there");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast!");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was fast!");
+});
 
-  app.listen(3000, () => {
-    console.log("App is listening on port 3000...");
-  });
-}
+app.listen(3000, () => {
+  console.log("App is listening on port 3000...");
+});
