96ed6efda2859064ccc9c9b1b52189ccb04e56bd
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-11 19:45:00 +0300

    Add clustering example

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
-const express = require("express");
-const app = express();
 
 // Is the file being executed in master mode?
 if (cluster.isMaster) {
-  // Execute again in child mode
+  // Execute index.js again in child mode
+  cluster.fork();
   cluster.fork();
 } else {
   // Child that acts like a server
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
+  const crypto = require("crypto");
+  const express = require("express");
+  const app = express();
 
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there");
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast!");
   });
 
   app.listen(3000, () => {
